764b2cedcc6e8c8f855d9b98026964317f676c52
Antonino Ragusa <antonino.ragusa.er@gmail.com>
2025-05-16 12:46:03 +0200

    Update menu-loader.js

--- menu-loader.js
+++ menu-loader.js
@@ -1,23 +1,26 @@
-function formatLabel(filename) {
-  const name = filename.replace('.md', '');
-  return name.replace(/_/g, ' ')
-             .replace(/([a-z])([A-Z])/g, '$1 $2')
-             .replace(/\b\w/g, c => c.toUpperCase());
-}
-
-fetch('/.netlify/functions/listFiles')
-  .then(res => res.json())
+fetch("/.netlify/functions/listFiles")
+  .then(res => {
+    if (!res.ok) throw new Error("Errore HTTP: " + res.status);
+    return res.json();
+  })
   .then(files => {
-    const menu = document.getElementById('menu');
+    if (!Array.isArray(files)) {
+      throw new Error("Risposta non è un array");
+    }
+    const menuList = document.getElementById("menu-list");
+    menuList.innerHTML = ""; // pulisci lista
+
     files.forEach(file => {
-      const li = document.createElement('li');
-      const a = document.createElement('a');
+      const li = document.createElement("li");
+      const a = document.createElement("a");
       a.href = `viewer.html?file=${encodeURIComponent(file)}`;
-      a.textContent = formatLabel(file);
+      a.textContent = file.replace(".md", "");
       li.appendChild(a);
-      menu.appendChild(li);
+      menuList.appendChild(li);
     });
   })
   .catch(err => {
-    console.error('Errore nel caricamento del menu:', err);
+    console.error("Errore nel caricamento del menu:", err);
+    const menuList = document.getElementById("menu-list");
+    menuList.innerHTML = `<li>Errore nel caricamento del menu: ${err.message}</li>`;
   });
